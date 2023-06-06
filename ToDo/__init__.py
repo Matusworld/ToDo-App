28972d426ba12fb3d04ff94db4aff70319ab3d14
matusworld <frommelt@live.dk>
2023-06-06 13:07:16 +0200

    Delete of note comments

--- ToDo/__init__.py
+++ ToDo/__init__.py
@@ -24,15 +24,3 @@ from ToDo.Login.routes import Login
 
 app.register_blueprint(Views)
 app.register_blueprint(Login)
-
-# Task is still not implemented
-# View button needs to be implemented
-# Delete button needs to be implemented
-
-# A interesting SQL query could be to get all the tasks that a user has to do, diffent route for this called all tasks
-
-# design is ugly xD
-
-# small things:
-# would be nice if usernames was not case sensitive
-# add a trigger that logs every sql query that is executed and who executed it, and what time it was executed, and what the result was
